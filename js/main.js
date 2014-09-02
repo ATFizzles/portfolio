82fae4d094dfc834fdf3a4036316c00ca39c1031
Axel Tanner <axeltanner38@gmail.com>
2014-09-02 12:52:42 -0700

    new new

--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
-(function(){
- 
-var counter = 0,
-$items = document.querySelectorAll('.diy-slideshow figure'),
-numItems = $items.length;
- 
-var showCurrent = function(){
-var itemToShow = Math.abs(counter%numItems);
- 
-[].forEach.call( $items, function(el){
-el.classList.remove('show');
-});
- 
-$items[itemToShow].classList.add('show');
-};
- 
-document.querySelector('.next').addEventListener('click', function() {
-counter++;
-showCurrent();
-}, false);
- 
-document.querySelector('.prev').addEventListener('click', function() {
-counter--;
-showCurrent();
-}, false);
- 
-})();
+$(function() {
+  var container = $('#container');
+  var toggle = $('#toggle');
+
+  toggle.on('click', function(){
+  container.toggleClass('open');
+  toggle.toggleClass('rotate');
+  });        
+})  
